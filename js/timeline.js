205ec55d651eb77356b2a171433ffe7bc8531033
JianyangZhang <jianyang212@outlook.com>
2016-12-15 01:53:58 -0800

    code refine
    code refine

--- js/timeline.js
+++ js/timeline.js
@@ -1,16 +1,17 @@
-window.onload = function() {
+window.onload = function () {
 	var curIndex = 0;
 	var timeLine = document.getElementById("timeline");
 	var clickArea = timeLine.getElementsByTagName("s");
 	var timePoint = timeLine.getElementsByTagName("li");
+	var len = clickArea.length;
 
-	for (var i = 0, len = clickArea.length; i < len; i++) {
-		(function(i) {
-			clickArea[i].onclick = function() {
+	for (var i = 0; i < len; i++) {
+		(function (i) {
+			clickArea[i].onclick = function () {
 				timePoint[i].className = "active";
 				timePoint[curIndex].className = "";
 				curIndex = i;
-			};
-		})(i);
+			}
+		}) (i);
 	}
-};
+}
